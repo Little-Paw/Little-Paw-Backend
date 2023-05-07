63ab07c63ec75f12556896c17b44d69f4541e7f2
Dylan-Chambi <dylanchambifi@gmail.com>
2023-05-07 10:26:15 -0400

    Replacing Firebase json credentials file with .env variables

--- src/config/serviceAccount.ts
+++ src/config/serviceAccount.ts
@@ -1,4 +1,20 @@
-import * as admin from 'firebase-admin';
-import * as serviceAccount from './firebase-application-credentials.json';
+import dotenv from 'dotenv';
 
-export default serviceAccount as admin.ServiceAccount;
+dotenv.config();
+
+const serviceAccount = JSON.parse(
+  `{ \
+    "type": "${process.env.FIREBASE_TYPE}", \
+    "project_id": "${process.env.FIREBASE_PROJECT_ID}", \
+    "private_key_id": "${process.env.FIREBASE_PRIVATE_KEY_ID}", \
+    "private_key": "${process.env.FIREBASE_PRIVATE_KEY}", \
+    "client_email": "${process.env.FIREBASE_CLIENT_EMAIL}", \
+    "client_id": "${process.env.FIREBASE_CLIENT_ID}", \
+    "auth_uri": "${process.env.FIREBASE_AUTH_URI}", \
+    "token_uri": "${process.env.FIREBASE_TOKEN_URI}", \
+    "auth_provider_x509_cert_url": "${process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL}", \
+    "client_x509_cert_url": "${process.env.FIREBASE_CLIENT_X509_CERT_URL}" \
+    }`,
+);
+
+export default serviceAccount;
